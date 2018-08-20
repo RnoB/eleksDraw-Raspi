2ffbf7f9fbfb44729ea029121eeb4790bf035b20
rnoB <rno.bsti@gmail.com>
2018-08-20 21:39:12 +0200

    typo

--- src/drawer.py
+++ src/drawer.py
@@ -1,8 +1,10 @@
 import serial
 import time
+import matj-h
 
 s = []
-
+x = 0
+y = 0
 def intializeDrawer():
     global s
     s = serial.Serial('/dev/ttyUSB0',115200)
@@ -10,10 +12,21 @@ def intializeDrawer():
     time.sleep(2)
     s.flushInput()
 
-def closeDrawer():
+
+def sendCommand(gCode):
+    print(gCode)
     s.flushInput()
-    s.write(('G1X0Y0F3500'.strip()+'\r\n').encode('UTF-8'))
-    s.readline()
+    s.write(gCode)
+    out = s.readline()
+    print(out)
+
+def toPosition(x0,y0,speed = 3500)
+    gCode = (('G1X'+str(x0)+'Y'+str(y0)+'F'+speed).strip()+'\r\n').encode('UTF-8')
+    sendCommand(gCode)
+    
+
+def closeDrawer():
+    toPosition(0,0)
     s.flushInput()
     s.write(('M30'.strip()+'\r\n').encode('UTF-8'))
     s.readline()
@@ -28,6 +41,11 @@ def penDown():
     s.flushInput()
     s.write(('M3S30'.strip()+'\r\n').encode('UTF-8'))
     s.readline()
+
+def line(x0,y0,length,angle):
+    
+    xf = x+L*math.cos(angle)
+
 
 def main():
     intializeDrawer()
