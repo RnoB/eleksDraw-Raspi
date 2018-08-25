842b0be89292cb8c2b243a24d9d2cd8d1214db30
rnoB <rno.bstin@gmail.com>
2018-08-25 18:44:12 +0200

    added lines

--- src/test.py
+++ src/test.py
@@ -59,7 +59,7 @@ def main():
     try:
         for k in np.range(0,10,20):
             x = np.arange(20,120,.2)
-            draw.lines(x,30+k+2*np.sin(x^(1+k/5))
+            draw.lines(x,30+k+2*np.sin(x^(1+k/5)))
         #line(50,50,length=50,angle=0)
         #line(50,50,length=55,angle=.1)
         #line(50,50,length=60,angle=.2)
