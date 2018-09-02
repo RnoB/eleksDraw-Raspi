1e777f39e2ec25d785d603e6cd7c4f151bec1d7e
rnoB <rno.bstin@gmail.com>
2018-09-02 12:06:47 +0200

    more test

--- src/test3.py
+++ src/test3.py
@@ -94,7 +94,7 @@ def main():
     try:
         for j in range(0,3):
             for l in range(0,2):
-                nLines = 50*(3*l+j+1)
+                nLines = 75*(3*l+j+1)
 
                 #z = frames[j]
                 #A = angle[j]
