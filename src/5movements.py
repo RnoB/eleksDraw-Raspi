e3226ac9656b566d7e966f3b1e888b82c4aedc6f
rnoB <rno.bstin@gmail.com>
2019-02-28 12:51:00 +0100

    first finalized version

--- src/5movements.py
+++ src/5movements.py
@@ -86,7 +86,7 @@ def round(x, base=1):
     return base * np.round(x/base)
 
 
-def draw(kFrames,frames,angle,angleZ,nLines = 400,scale = 70,A0=0,rounder=.1,noise = 0,offsetX = 0,offsetY=0,X2 = []):
+def drawing(kFrames,frames,angle,angleZ,drawer,nLines = 400,scale = 70,A0=0,rounder=.1,noise = 0,offsetX = 0,offsetY=0,X2 = []):
     X3 = []
     X = []
 
@@ -236,7 +236,7 @@ def main():
             offsetY = offsetY0+j*dist
 
 
-            draw(kFrames,kinect.frames,angle,angleZ,nLines = nLines,scale = 70,A0=0,rounder=rounder,noise = 0.5,offsetX = offsetX,offsetY=offsetY,X2 = X2)
+            drawing(kFrames,kinect.frames,angle,angleZ,draw,nLines = nLines,scale = 70,A0=0,rounder=rounder,noise = 0.5,offsetX = offsetX,offsetY=offsetY,X2 = X2)
             
 
     except Exception as e: 
