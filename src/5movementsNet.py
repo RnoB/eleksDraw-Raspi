4b70302a63c984cedb17e35120831fa52b51b4dc
rnoB <rno.bstin@gmail.com>
2019-10-31 14:22:14 +0100

    colors

--- src/5movementsNet.py
+++ src/5movementsNet.py
@@ -136,13 +136,13 @@ def drawing(kFrames,frames,angle,angleZ,draw,
             while xChecking:
                 kx0 = random.randint(0, 639)
                 ky0 = random.randint(0, 479)
-                x,y = scaler(kx,ky,scale=scale,offsetX=offsetX,offsetY=offsetY)
+                x,y = scaler(kx0,ky0,scale=scale,offsetX=offsetX,offsetY=offsetY)
                 x0 = round(x+(.5-random.random())*xu,resolution/2.0)
                 y0 = round(y+(.5-random.random())*yu,resolution/2.0)
                 x = x0
                 y = y0
-                kx=kx0
-                ky=ky0
+                kx = kx0
+                ky = ky0
                 x1 = round(x,distanceLine)
                 y1 = round(y,distanceLine)
                 x2 = round(x,distanceFigure)
