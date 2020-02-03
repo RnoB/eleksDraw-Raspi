423126df7532ed0111b7a247bb3fbf72be7bf94d
rnoB <rno.bstin@gmail.com>
2020-02-03 17:35:08 +0100

    added polar movements +inversion of penUp penDown

--- src/mouseEvent.py
+++ src/mouseEvent.py
@@ -5,22 +5,26 @@ dev = InputDevice('/dev/input/event0')
 async def helper(dev):
      async for ev in dev.async_read_loop():
         if ev.type == 1:
-            print("Mouse Pressed")
+            print("--- Click ---")
             if ev.value == 0:
                 print("Released")
             elif ev.value == 1:
                 print("Pressed")
             if ev.code == 272:
                 print("Left")
-            elif ev.code ==273:
-                print("Middle")
             elif ev.code ==274:
+                print("Middle")
+            elif ev.code ==273:
                 print("Right")
         if ev.type == 2:
-            print("Movement")
-            print(" Code : "+str(ev.code)+" value : "+str(ev.value))
-        if ev.type == 4:
-            print("No idea")
-            print(" Code : "+str(ev.code)+" value : "+str(ev.value))
+            print("-- Movement --")
+            if ev.code == 0:
+                print("X : "+str(ev.value))
+            if ev.code == 1:
+                print("Y : "+str(ev.value))
+            if ev.code == 6:
+                print("WheelX : "+str(ev.value))
+            if ev.code == 8:
+                print("WheelY : "+str(ev.value))
 loop = asyncio.get_event_loop()
 loop.run_until_complete(helper(dev))
